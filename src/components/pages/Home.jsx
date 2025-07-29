d976122f8d8f5a4dde38e82ffb5060e5643b8e34
Santhosh-609 <dedsecsanti@gmail.com>
2025-07-29 18:45:40 +0530

    ui reworks 1

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,14 @@
-// components/Home.jsx
 import HeroSection from '../HeroSection';
 import Cities from '../Cities';
 import OurVehicles from '../OurVehicles';
 import OurServices from '../OurServices';
 import WhatsAppButton from '../WhatsappButton';
+// import SubHeader from '../SubHeader';
 
 const Home = () => {
   return (
     <>
+      {/* <SubHeader /> */}
       <HeroSection />
       <OurServices />
       <OurVehicles />
