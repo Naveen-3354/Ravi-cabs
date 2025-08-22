a67938401be7a6cb8f2de09b9db3feec1772a006
Naveen-3354 <welbe1709@gmail.com>
2025-08-22 20:30:16 +0530

    added extra columns

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -256,6 +256,7 @@ const Form = ({activeTab, setActiveTab, toast}) => {
     if (showEstimation) {
         return (
             <div
+                id="my-section"
                 className="mt-8 rounded-xl text-white p-4 md:p-6 w-full max-w-xl relative z-20 border border-gray-700/50 overflow-hidden">
                 <div
                     className="absolute inset-0 bg-[url('/images/highway_1.jpg')] bg-cover bg-center opacity-20 z-0"></div>
